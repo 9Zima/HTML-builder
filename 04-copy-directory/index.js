a372fdc2c94254fe3faf3efca2352ffa1bb74986
9Zima <blazingsword404@gmail.com>
2022-05-24 09:59:49 +0300

    feat: complete 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fsProm = require('fs/promises');
+const path = require('path');
 
+let folderPath = path.join(__dirname, './files');
+let copiedFolderPath = path.join(__dirname, './files-copy');
+
+
+async function deleteOldFiles() {
+  await fsProm.mkdir(copiedFolderPath, {recursive: true});
+  let oldFilesList = await fsProm.readdir(copiedFolderPath, {withFileTypes: true});
+  for (let file of oldFilesList)
+    fsProm.rm(path.join(copiedFolderPath, file.name));
+  copieFiles();
+}
+
+async function copieFiles() {
+  let filesList = await fsProm.readdir(folderPath, {withFileTypes: true});
+  for (let file of filesList)
+    await fsProm.copyFile(path.join(folderPath, file.name), path.join(copiedFolderPath, file.name));
+}
+
+deleteOldFiles();
+      
